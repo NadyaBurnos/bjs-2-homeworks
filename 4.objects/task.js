7d279a1a4329c926c85fff7baaa24d0b838581e1
NadyaBurnos <153112000+NadyaBurnos@users.noreply.github.com>
2024-02-27 23:29:06 +0700

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(this.marks === undefined){
+        this.marks = [...marks];   
+    } else {
+        this.marks = this.marks.concat(marks);
+    }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+    if(this.marks === undefined){
+        return 0;
+    } else {
+        return this.marks.reduce((acc, value) => acc + value / this.marks.length, 0 );
+    }
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.exclude = reason;
 }
